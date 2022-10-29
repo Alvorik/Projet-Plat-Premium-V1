db455273079dc84fc07157af2dea7506f29a2013
Alvorik <115509470+Alvorik@users.noreply.github.com>
2022-10-28 20:20:53 -0500

    Update ProjectPlatinumPremium.lua

--- ProjectPlatinumPremium.lua
+++ ProjectPlatinumPremium.lua
@@ -21,18 +21,6 @@ MainSection:NewButton("Admin Commands #2", "i have no idea what the name of the 
     loadstring(game:HttpGet('\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\79\109\110\105\112\111\116\101\110\99\101\68\101\118\101\108\111\112\101\114\47\78\117\109\98\101\114\47\109\97\105\110\47\49\46\108\117\97'))()
 end)
 
--- Updates Etc.
-local Upd = Window:NewTab("Upd, Info")
-local UpdSection = Hub:NewSection("Updates Info | Project Plat Premium")
-
-UpdSection:NewButton("Creator Etc.", "Script Created by Alvorikk", function()
-	print(ScriptCreatedByAlvorik)
-end)
-
-UpdSection:NewButton("Update V5", "Welcome to Project Plat Premium Official ScriptHub", function ()
-	print(UpdV5Info)
-end)
-
 -- Hub Section
 local Hub = Window:NewTab("Hub")
 local HubSection = Hub:NewSection("Hubs | Premium")
